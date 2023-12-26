f2eb58fa4c03911ee036b482df724e9591f2eaf3
Artem Bukharov <artem.bukharov.2018@gmail.com>
2023-12-27 00:11:55 +0300

    Evennoteven

--- src/Evennoteven.java
+++ src/Evennoteven.java
@@ -1,2 +1,14 @@
+import  java.util.Scanner ;
 public class Evennoteven {
-}
+    public static void main(String[] args) {
+        Scanner win = new Scanner(System.in);
+        System.out.println("Введите число");
+        int number = win.nextInt();
+        if (number%2==0 ){
+            System.out.println("Чётное");}
+            else {
+                System.out.println("Нечётное");}
+            }
+        }
+
+
