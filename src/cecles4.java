99c86a61a57758a7349ab1bcd2c3324390a340a6
Artem Bukharov <artem.bukharov.2018@gmail.com>
2024-01-02 21:55:54 +0300

    cycles4

---
+++ src/cecles4.java
@@ -0,0 +1,12 @@
+public class cecles4 {
+    public static void main(String[] args) {
+        int number = 7;
+        int result = number;
+        int kvadrat = 1;
+        while (result <= 98) {
+            System.out.println( result);
+            kvadrat++;
+            result=number * kvadrat;
+        }
+    }
+}
