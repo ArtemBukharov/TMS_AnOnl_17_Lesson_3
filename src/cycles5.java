901b8671fafddf939a230d0bdb2cab95af9e0bcf
Artem Bukharov <artem.bukharov.2018@gmail.com>
2024-01-02 22:01:30 +0300

    cycles5

---
+++ src/cycles5.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class cycles5 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i >= -50) {
+            System.out.println(i);
+            i = i-5;
+        }
+    }
+}
