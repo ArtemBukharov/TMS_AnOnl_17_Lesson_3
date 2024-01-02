7e49b39fd02903e62adab8101add63dfa878b0a6
Artem Bukharov <artem.bukharov.2018@gmail.com>
2024-01-02 22:14:22 +0300

    cycles6

---
+++ src/cycles6.java
@@ -0,0 +1,9 @@
+public class cycles6 {
+    public static void main(String[] args) {
+        int i = 0;
+        int kvadrat = 2;
+        for (i = 10; i <= 20; i++) {
+            System.out.println(i*kvadrat);
+        }
+    }
+}
